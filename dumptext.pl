c4b9e1c75b0c3ddb71dc35b27dfb9e757fd8a6bf
llamasoft <gravisoft@gmail.com>
2016-04-29 21:40:13 -0500

    Getopt and POD documentation

--- dumptext.pl
+++ dumptext.pl
@@ -4,45 +4,80 @@ use strict;
 use warnings;
 no  warnings 'utf8';
 
+use Getopt::Long qw{ :config auto_help };
+use Pod::Usage;
+use Data::Dumper;
+
 use threads;
 use threads::shared;
 use Thread::Queue;
+use Time::HiRes;
 
 use Encode;
 use HTML::Entities;
 
-# First parameter is XML file, second parameter are categories
-my $xml_name = shift(@ARGV) or die "Expected a MediaWiki dump file as parameter";
 
-# Transclusions of interest:
-#   {{good article}}, {{featured article}}
-# Categories of interest:
-#   {music, albums, songs}, {television, films}, {books, novels}, video games, {websites, internet meme}
-#   sports, {ancient, history, chronology}
-my @categories = map { qr/\Q$_\E/i; } @ARGV;
-if ( scalar(@categories) == 0 ) { die "Expected at least one article category as a parameter"; }
-printf(STDERR "Using categories: %s\n", join(', ', @ARGV));
+
+# First parameter is XML file, second parameter are categories
+my $xml_name;
+my @categories;
+my @transclusions;
+my $keep_all = 0;
+my $worker_count = 2;
+
+
+GetOptions(
+    "xml=s"                 => \$xml_name,
+    "workers=i"             => \$worker_count,
+    "categories=s{1,}"      => \@categories,
+    "transclusions=s{1,}"   => \@transclusions,
+    "help|?"                => sub { pod2usage({ -verbose => 3 }); }
+);
+
+
+# Make sure --xml was included
+if ( !defined($xml_name) || length($xml_name) == 0 ) {
+    pod2usage("Input file must be specified with --xml option\n");
+}
+
+
+# If no filtering options were supplied, keep all articles
+if ( scalar(@categories) == 0 && scalar(@transclusions) == 0 ) {
+    printf(STDERR "No filtering applied, all articles will be kept\n");
+    $keep_all = 1;
+
+} else {
+    # Otherwise, convert transclusions and categories to static regular expressions
+    if ( scalar(@categories) > 0 ) {
+        printf(STDERR 'Including categories: "%s"\n', join('", "', @categories));
+        @categories = map { qr/\Q$_\E/i; } @categories;
+    }
+
+    if ( scalar(@transclusions) > 0 ) {
+        printf(STDERR 'Including transclusions: "%s"\n', join('", "', @transclusions));
+        @categories = map { qr/\Q$_\E/i; } @transclusions;
+    }
+}
 
 
 # Number of worker threads to create and a dummy variable to act as a print lock
-my $thread_count = 2;
 my $stdout_lock :shared;
 my $stderr_lock :shared;
 
 # Work queue and maximum number of queue entries before stalling the producer thread
-my $thread_queue = Thread::Queue->new();
-my $thread_queue_max = 1000;
-my $thread_results = Thread::Queue->new();
+my $worker_queue   = Thread::Queue->new();
+my $worker_results = Thread::Queue->new();
+my $worker_queue_max = 4 * 1024;
 
-# Our array of threads and thread creation
-my @threads = ();
-for ( 1 .. $thread_count ) { push(@threads, threads->create(\&worker_thread, $_)); }
+# Our array of workers and worker thread creation
+my @workers = ();
+for ( 1 .. $worker_count ) { push(@workers, threads->create(\&worker_thread, $_)); }
 
 
 # The XML file contains UTF-8, but specifying :utf8 encoding causes almost all
 #   string functions (including regex, read, etc) to be almost 20x slower
 # Instead, we lie to perl here, then later decode() the article to UTF-8 for processing
-open(my $xml, '<', $xml_name) || die("Failed to open MediaWiki XML Dump ${xml_name}");
+open(my $xml, '<', $xml_name) || die("Failed to open MediaWiki XML dump ${xml_name}");
 my $buffer = "";
 
 my $total  = 0;
@@ -63,7 +98,7 @@ while( (my $read_bytes = read($xml, $buffer, 64 * 1024, length($buffer))) > 0 ) 
         my $page = $1;
 
         # Yes, I'm parsing XML with Regular Expressions
-        # MediaWiki dumps are very clean and real XML parsing is way slower (20x)
+        # MediaWiki dumps are very clean and even XML::Simple parsing is way slower (20x)
         # Skip this page unless it has a title, namespace, and text body
         next unless $page =~ m{
             <title>(?'title'.*?)</title> .*?
@@ -76,36 +111,41 @@ while( (my $read_bytes = read($xml, $buffer, 64 * 1024, length($buffer))) > 0 ) 
         my $namespace = int( $+{'namespace'} );
         my $redirect  = defined($+{'redirect'});
         my $text      = $+{'text'};
-        $total++;
-
-
-        if ( !defined($namespace) || !defined($text) ) {
-            print STDERR "Got strange article:", $/, $page, $/, $/;
-            exit;
-        }
-
 
         # Skip special articles (lists, categories, help pages)
         if ( $namespace != 0 ) { next; }
         if ( $redirect       ) { next; }
+        $total++;
 
-        ############ Keep Criteria #############
-        # Only articles in our target categories
-        my $keep = 0;
-        while ( $text =~ /\[\[ Category: ([^\]]+) \]\]/xig ) {
+
+        ########## Article Filtering ###########
+        my $keep = $keep_all;
+
+        # First check for categories
+        while ( !$keep && $text =~ /\[\[ Category: ([^\]]+) \]\]/xig ) {
             my $cur_category = $1;
+
             for my $target_category (@categories) {
                 if ( $cur_category =~ $target_category ) { $keep = 1; last; }
             }
         }
+
+        # Then check transclusions
+        while ( !$keep && $text =~ /\{\{ ([^\}]+) \}\}/xig ) {
+            my $cur_transclusion = $1;
+            for my $target_category (@categories) {
+                if ( $cur_transclusion =~ $target_category ) { $keep = 1; last; }
+            }
+        }
+
         if ( $keep == 0 ) { next; }
         ########################################
-        $parsed++;
 
 
         # Queue the work and stall if threads are overloaded
-        $thread_queue->enqueue($text);
-        while ($thread_queue->pending() >= $thread_queue_max) { sleep(1); }
+        $parsed++;
+        $worker_queue->enqueue($text);
+        while ($worker_queue->pending() >= $worker_queue_max) { usleep(0.1 * 1000000); }
 
         # Print any pending results
         print_queue();
@@ -141,8 +181,8 @@ while( (my $read_bytes = read($xml, $buffer, 64 * 1024, length($buffer))) > 0 ) 
 close($xml);
 
 # Signal end-of-work and wait for queue to finish
-$thread_queue->end();
-for my $thread (@threads) { $thread->join(); }
+$worker_queue->end();
+for my $worker (@workers) { $worker->join(); }
 print_queue();
 
 
@@ -158,8 +198,8 @@ printf(STDERR "[%s] Kept %d out of %d articles (%.2f%%)\n",
 #################### Utility Functions #####################
 
 sub print_queue {
-    while ( $thread_results->pending() ) {
-        print STDOUT $thread_results->dequeue(), $/;
+    while ( $worker_results->pending() ) {
+        print STDOUT $worker_results->dequeue(), $/;
     }
 }
 
@@ -173,7 +213,7 @@ sub worker_thread {
         select(STDERR)->flush();
     }
 
-    while ( defined(my $text = $thread_queue->dequeue()) ) {
+    while ( defined(my $text = $worker_queue->dequeue()) ) {
         # Strip any footers or reference sections
         # The shorter we make the article early on, the faster the processing
         $text =~ s/
@@ -242,7 +282,7 @@ sub worker_thread {
         $text =~ s/[\r\n]+/\n/g;                            # Condense blank lines, remove carriage returns
 
 
-        $thread_results->enqueue($text);
+        $worker_results->enqueue($text);
     }
 }
 
@@ -276,3 +316,110 @@ sub parse_short_link {
     my @link_parts = split(/\s+/, $link_body, 2);
     return pop(@link_parts);
 }
+
+
+
+__END__
+=head1 NAME
+
+B<WikiDump> - Processes a WikiMedia XML dump file and prints the stripped markup text.
+
+=head1 SYNOPSIS
+
+B<wikidump.pl> B<--xml> F<path/to/dump.xml> [B<--workers> N] [B<--categories> ...] [B<--transclusions> ...]
+
+Parsed articles are written to STDOUT, status updates are written to STDERR.
+Filtering can be applied to include only certain articles.  If no filtering options are specified, all articles are kept.
+
+B<Options>
+
+=over 4
+
+=item B<--xml>
+
+The MediaWiki XML dump to process, this option is required
+
+=item B<--workers>
+
+The number of worker threads to spawn (default 2)
+
+=item B<--categories>
+
+Articles whose categores contain any of these entries will be kept
+
+=item B<--transclusions>
+
+Articles whose transclusions contain any of these entires will be kept
+
+=item B<--help>
+
+Displays a full help guide including additional parameter details
+
+=back
+
+B<Examples>
+
+=over 4
+
+B<wikidump.pl> B<--xml> F<dump.xml> B<--transclusions> "featured article" "good article" > enwiki_quality.txt
+
+B<wikidump.pl> B<--xml> F<dump.xml> B<--categories> "novels" "books" > enwiki_books.txt
+
+B<wikidump.pl> B<--xml> F<dump.xml> > enwiki_everything.txt
+
+=back
+
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<--xml> F<path>
+
+The path to the MediaWiki XML dump file to be processed.
+
+
+=item B<--workers N>
+
+The number of worker threads spawned to parse MediaWiki markdown.  By default, B<2> workers are used.
+
+Workers are I<only> responsible for parsing the MediaWiki markdown into plain text, not filtering articles.
+Filters that result in low keep rates may result in underutilized workers.
+
+
+=item B<--categories  "string1"  "string2"  ...>
+
+Articles whose categories contain I<any> of these case-insensitive strings will be kept.
+
+An article's categories are determined by the C<[[Category:]]> tags that its markdown contains.
+Multiple values may be defined by separating them with spaces (e.g. I<"space travel"> I<"NASA">).
+
+Information about markdown categories can be found here: L<https://en.wikipedia.org/wiki/Help:Category>
+
+
+=item B<--transclusions  "string1"  "string2"  ...>
+
+Articles whose transclusions contain I<any> of these case-insensitive strings will be kept.
+
+An article's transclusions are determined by the C<{{ ... }}> tags that its markdown contains.
+Multiple values may be defined by separating them with spaces (e.g. I<"featured article"> I<"good article">).
+
+Information about markdown transclusions can be found here: L<https://en.wikipedia.org/wiki/Wikipedia:Transclusion>
+
+=back
+
+
+
+=head1 DESCRIPTION
+
+This program processes a MediaWiki XML dump file and prints the stripped markup text to STDOUT and status messages to STDERR.
+
+Filtering with B<--categories> or B<--transclusions> can narrow down the articles printed, useful for generating topical word frequencies, wordlists, or N-grams.
+Both B<--categories> and B<--transclusions> use inclusive filtering.  If any string from either option results in a match, the article will be kept.
+If no filtering criteria is specified, all articles will be kept.
+
+The latest English Wikipedia XML dumps can be found at L<https://dumps.wikimedia.org/enwiki/latest/>.
+
+This program was created to parse F<enwiki-latest-pages-articles.xml>, but may parse other files as well.  Your mileage may vary.
+
+=cut
